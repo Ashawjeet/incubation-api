d63186699ffe901445ed4dd35c3b0000f8c6e712
ashawjeet <ashawjeetshinde2016@gmail.com>
2024-08-28 10:58:40 +0530

    ds

--- config/db.js
+++ config/db.js
@@ -15,27 +15,27 @@
 
 
 
+//final 28
+// const mongoose = require("mongoose");
 
-const mongoose = require("mongoose");
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log("MongoDB connected...");
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB connected...");
+//     // Test the database connection
+//     const connectionStatus = await mongoose.connection.db.admin().ping();
+//     console.log("MongoDB connection status:", connectionStatus);
+//   } catch (err) {
+//     console.error("Database connection error:", err.message);
+//     process.exit(1);
+//   }
+// };
 
-    // Test the database connection
-    const connectionStatus = await mongoose.connection.db.admin().ping();
-    console.log("MongoDB connection status:", connectionStatus);
-  } catch (err) {
-    console.error("Database connection error:", err.message);
-    process.exit(1);
-  }
-};
-
-module.exports = connectDB;
+// module.exports = connectDB;
 
 
 
@@ -56,3 +56,22 @@ module.exports = connectDB;
 
 // module.exports = connectDB;
  
+
+
+
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true, // Can be removed
+      useUnifiedTopology: true, // Can be removed
+    });
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error('Database connection error:', err.message);
+    process.exit(1); // Exit the process with failure
+  }
+};
+
+module.exports = connectDB;
